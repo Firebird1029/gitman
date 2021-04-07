0d0c786b1407361b9a1434df64470325c3e04d19
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2021-04-06 22:44:19 -1000

    code lint

--- src/setup.ts
+++ src/setup.ts
@@ -2,7 +2,7 @@ import { Clone, Cred } from "nodegit";
 import * as path from "path";
 
 (async () => {
-	const repo = await Clone.clone(process.env.GIT_REPO, path.join(__dirname, "../", "repo"), {
+	const repo = await Clone.clone(process.env.GIT_REPO, path.join(__dirname, "../repo"), {
 		fetchOpts: {
 			callbacks: {
 				certificateCheck: () => 0,
