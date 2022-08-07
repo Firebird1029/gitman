e8d1359bb54690ea969293f525216f61ed40d798
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-08-06 17:42:25 -1000

    add threshold to midnight schedule

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import * as cron from "node-cron";
 import runWorkflow from "./workflow";
 
 // Every day at midnight
-cron.schedule("0 0 * * *", runWorkflow);
+cron.schedule("0 0 * * *", () => {
+	if (Math.random() < Number(process.env.MIDNIGHT_RANDOM_THRESHOLD)) {
+		runWorkflow();
+	}
+});
 
 // Every hour
 cron.schedule("0 * * * *", () => {
-	if (Math.random() > Number(process.env.RANDOM_THRESHOLD)) {
+	if (Math.random() < Number(process.env.HOUR_RANDOM_THRESHOLD)) {
 		runWorkflow();
 	}
 });
